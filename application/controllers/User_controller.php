4abf6cb37413ca593ad1ef94c74424d8747999ba
rzatalibov555 <sethubpro@gmail.com>
2023-04-22 18:16:39 +0400

    Barbershop start project

---
+++ application/controllers/User_controller.php
@@ -0,0 +1,23 @@
+<?php 
+
+// https://codeigniter.com/userguide3/general/urls.html
+class User_controller extends CI_Controller{
+
+    public function index(){
+        echo "Men index metoduyam.";
+    }
+
+    public function about(){
+        echo "Men about metoduyam.";
+    }
+    
+
+
+
+
+
+
+
+
+
+}
